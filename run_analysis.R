220beee113ea89362781cbed454a662ced1ce077
sharang17 <sharangbhat@hotmail.com>
2015-04-27 00:01:31 +0530

    completed course project

---
+++ run_analysis.R
@@ -0,0 +1,56 @@
+library(plyr)
+
+
+
+x_train <- read.table("train/X_train.txt")
+y_train <- read.table("train/y_train.txt")
+subject_train <- read.table("train/subject_train.txt")
+
+x_test <- read.table("test/X_test.txt")
+y_test <- read.table("test/y_test.txt")
+subject_test <- read.table("test/subject_test.txt")
+
+x_data <- rbind(x_train, x_test)
+
+y_data <- rbind(y_train, y_test)
+
+
+subject_data <- rbind(subject_train, subject_test)
+
+
+
+features <- read.table("features.txt")
+
+
+mean_and_std_features <- grep("-(mean|std)\\(\\)", features[, 2])
+
+
+x_data <- x_data[, mean_and_std_features]
+
+
+names(x_data) <- features[mean_and_std_features, 2]
+
+
+
+activities <- read.table("activity_labels.txt")
+
+# update values with correct activity names
+y_data[, 1] <- activities[y_data[, 1], 2]
+
+# correct column name
+names(y_data) <- "activity"
+
+
+
+# correct column name
+names(subject_data) <- "subject"
+
+# bind all the data in a single data set
+all_data <- cbind(x_data, y_data, subject_data)
+
+
+
+# 66 <- 68 columns but last two (activity & subject)
+averages_data <- ddply(all_data, .(subject, activity), function(x) colMeans(x[, 1:66]))
+
+write.table(averages_data, "averages_data.txt", row.name=FALSE)
